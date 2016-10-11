dae7232d6cd3e12fa97509c58f9608950f14a56b
Z <Z@Z-PC>
2016-10-11 14:59:35 +0800

    第一作業程式碼

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore"; 
+		String lyrics1 =  lyrics.replace("it","\"her\"");
+		String lyrics2 =  lyrics1.replace("Cannot","Can't");
+		System.out.println(lyrics2);
+
+
+	}
+
+}
